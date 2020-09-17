267b65b766a30f890e9f29c5da592c149a0ced76
Roberto-Livi <robertomlivi@outlook.com>
2020-09-17 10:52:25 -0400

    added a Point class with three methods

--- Program.cs
+++ Program.cs
@@ -14,9 +14,12 @@ namespace csharp_intermediate
             var order = new Order();
             customer.Orders.Add(order);
 
-            System.Console.WriteLine(customer.Id);
-            System.Console.WriteLine(customer.Name);
+            // System.Console.WriteLine(customer.Id);
+            // System.Console.WriteLine(customer.Name);
 
+            var point = new Point(10, 20);
+            point.Move(new Point(40, 60));
+            System.Console.WriteLine("Point is at ({0} {1})", point.X, point.Y);
         }
     }
 }
